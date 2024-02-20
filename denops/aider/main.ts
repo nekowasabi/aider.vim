2a585a4124c212e3f0e44a96798190abed69ecea
nekowasabi <nolifeking00@gmail.com>
2024-02-20 19:46:55 +0900

    feat: Aider機能の実装とコマンド追加

--- denops/aider/main.ts
+++ denops/aider/main.ts
@@ -5,12 +5,24 @@ import { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
+    // ウインドウを分割して、現在のファイルを指定してaider起動
+    async runAider(): Promise<void> {
+      // 分割方向を判定
+      // 分割
+      // 現在のファイル取得
+      // aider起動
+    },
     // プロンプト送信
     async sendPrompt(prompt: unknown): Promise<void> {
       console.log(prompt);
+      // プロンプトを取得
+      // コマンドを合成
+      // aiderのウインドウに送信
     },
     // 現在のファイルをaddする
     async addCurrentFile(): Promise<void> {
+      // 現在のファイルを取得
+      // aiderのウインドウにaddする
     },
   };
 
@@ -19,5 +31,8 @@ export async function main(denops: Denops): Promise<void> {
   );
   await denops.cmd(
     `command! -nargs=0 AiderAddCurrentFile call denops#notify("${denops.name}", "addCurrentFile")`,
+  );
+  await denops.cmd(
+    `command! -nargs=0 AiderRun call denops#notify("${denops.name}", "runAider")`,
   );
 }
