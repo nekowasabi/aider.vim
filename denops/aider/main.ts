87fb452519c88f99f6baf6d52ff18d862bcd3d1d
nekowasabi <nolifeking00@gmail.com>
2024-02-24 18:14:46 +0900

    feat: AiderRun()を追加 :+1:

--- denops/aider/main.ts
+++ denops/aider/main.ts
@@ -1,16 +1,34 @@
 import { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
-// import * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
-// import * as v from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
-// import { is } from "https://deno.land/x/unknownutil@v3.14.0/mod.ts";
+import * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
+import * as v from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.14.0/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
-    // ウインドウを分割して、現在のファイルを指定してaider起動
-    async runAider(): Promise<void> {
-      // 分割方向を判定
-      // 分割
+    async splitWithDirection(): Promise<string> {
+      const splitDirection = ensure(
+        await v.g.get(denops, "aider_split_direction"),
+        is.String,
+      );
+      await denops.cmd(splitDirection ?? "split");
+      return splitDirection ?? "split";
+    },
+    async runAiderCommand(): Promise<void> {
       // 現在のファイル取得
+      const currentFile = ensure(
+        await fn.expand(denops, "%:p"),
+        is.String,
+      );
       // aider起動
+      const aiderCommand = ensure(
+        await v.g.get(denops, "aider_command"),
+        is.String,
+      );
+      await denops.cmd(`terminal ${aiderCommand} ${currentFile}`);
+    },
+    async runAider(): Promise<void> {
+      await this.splitWithDirection();
+      await this.runAiderCommand();
     },
     // プロンプト送信
     async sendPrompt(prompt: unknown): Promise<void> {
@@ -33,6 +51,6 @@ export async function main(denops: Denops): Promise<void> {
     `command! -nargs=0 AiderAddCurrentFile call denops#notify("${denops.name}", "addCurrentFile")`,
   );
   await denops.cmd(
-    `command! -nargs=0 AiderRun call denops#notify("${denops.name}", "runAider")`,
+    `command! -nargs=0 AiderRun call denops#notify("${denops.name}", "runAider", [])`,
   );
 }
