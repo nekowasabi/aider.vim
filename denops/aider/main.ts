b8c19a349143d793b7db5c4674a9842a0fc78a1f
nekowasabi <nolifeking00@gmail.com>
2024-02-28 21:18:34 +0900

    feat: 'sendPrompt'関数を更新

--- denops/aider/main.ts
+++ denops/aider/main.ts
@@ -20,18 +20,18 @@ export async function main(denops: Denops): Promise<void> {
   }
 
   async function forEachTerminalBuffer(
-    callback: (job_id: number, bufnr?: number) => Promise<void>,
+    callback: (job_id: number, winnr?: number, bufnr?: number) => Promise<void>,
   ): Promise<void> {
     const win_count = ensure(await fn.winnr(denops, "$"), is.Number);
     for (let i = 0; i <= win_count; i++) {
       const bufnr = ensure(await fn.winbufnr(denops, i), is.Number);
       if (await fn.getbufvar(denops, bufnr, "&buftype") === "terminal") {
         const job_id = ensure(
-          await fn.getbufvar(denops, bufnr, "terminal_job_id"),
+          await fn.getbufvar(denops, bufnr, "&channel"),
           is.Number,
         );
         if (job_id !== 0) {
-          await callback(job_id, bufnr);
+          await callback(job_id, i, bufnr);
         }
       }
     }
@@ -42,15 +42,26 @@ export async function main(denops: Denops): Promise<void> {
       await splitWithDirection();
       await this.runAiderCommand();
     },
-    async sendPrompt(prompt: unknown): Promise<void> {
+    async sendPrompt(): Promise<void> {
+      await feedkeys(denops, 'ggVG"zy');
+      await forEachTerminalBuffer(async (job_id, winnr, _bufnr) => {
+        await denops.cmd(`bdelete!`);
+        await denops.cmd(`${winnr}wincmd w`);
+        await feedkeys(denops, "G");
+        await feedkeys(denops, '"zp');
+        await denops.call("chansend", job_id, "\n");
+        await denops.cmd("wincmd p");
+      });
+    },
+    async sendPromptWithInput(prompt: unknown): Promise<void> {
       // promptが空なら何もしない
       if (prompt === "") {
         return;
       }
       const str = ensure(prompt, is.String) + "\n";
-      await forEachTerminalBuffer(async (job_id, bufnr) => {
+      await forEachTerminalBuffer(async (job_id, winnr) => {
         await denops.call("chansend", job_id, str);
-        await denops.cmd(`${bufnr}wincmd w`);
+        await denops.cmd(`${winnr}wincmd w`);
         await feedkeys(denops, "G");
         await denops.cmd("wincmd p");
       });
@@ -58,7 +69,7 @@ export async function main(denops: Denops): Promise<void> {
     async addCurrentFile(): Promise<void> {
       const currentFile = await getCurrentFilePath();
       const prompt = `/add ${currentFile}`;
-      await this.sendPrompt(prompt);
+      await this.sendPromptWithInput(prompt);
     },
     async runAiderCommand(): Promise<void> {
       const currentFile = await getCurrentFilePath();
@@ -69,7 +80,7 @@ export async function main(denops: Denops): Promise<void> {
       await denops.cmd(`terminal ${aiderCommand} ${currentFile}`);
     },
     async exitAider(): Promise<void> {
-      await forEachTerminalBuffer(async (job_id, bufnr) => {
+      await forEachTerminalBuffer(async (job_id, _winnr, bufnr) => {
         await denops.call("chansend", job_id, "/exit\n");
         await denops.cmd(`bdelete! ${bufnr}`);
       });
@@ -78,22 +89,35 @@ export async function main(denops: Denops): Promise<void> {
       start: unknown,
       end: unknown,
     ): Promise<void> {
-      const words = await denops.call("getline", start, end);
-      ensure(words, is.ArrayOf(is.String));
+      const words = ensure(await denops.call("getline", start, end), is.Array);
 
       // floatint window定義
       const buf = await n.nvim_create_buf(denops, false, true);
-      const win = await n.nvim_open_win(denops, buf, true, {
+      // 画面中央に表示
+      const terminal_width = Math.floor(
+        ensure(await n.nvim_get_option(denops, "columns"), is.Number),
+      );
+      const terminal_height = Math.floor(
+        ensure(await n.nvim_get_option(denops, "lines"), is.Number),
+      );
+      const row = Math.floor((terminal_height - 30) / 2);
+      const col = Math.floor((terminal_width - 100) / 2);
+
+      await n.nvim_open_win(denops, buf, true, {
         relative: "editor",
         border: "double",
-        width: 40,
-        height: 10,
-        row: 10,
-        col: 10,
+        width: 100,
+        height: 30,
+        row: row,
+        col: col,
       });
       await denops.cmd("setlocal buftype=nofile");
       await denops.cmd("set nonumber");
       await denops.cmd("set filetype=aider_prompt");
+
+      await n.nvim_buf_set_lines(denops, buf, -1, -1, true, words);
+      await n.nvim_buf_set_lines(denops, buf, 0, 1, true, []);
+      await n.nvim_buf_set_lines(denops, buf, -1, -1, true, [""]);
 
       // ウインドウを閉じる
       await n.nvim_buf_set_keymap(denops, buf, "n", "q", "<cmd>q!<cr>", {
@@ -114,13 +138,16 @@ export async function main(denops: Denops): Promise<void> {
   };
 
   await denops.cmd(
-    `command! -nargs=0 AiderSendPrompt call denops#notify("${denops.name}", "sendPrompt", [input("Prompt: ")])`,
+    `command! -nargs=0 AiderSendPrompt call denops#notify("${denops.name}", "sendPrompt", [])`,
   );
   await denops.cmd(
-    `command! -nargs=0 AiderAddCurrentFile call denops#notify("${denops.name}", "addCurrentFile", [])`,
+    `command! -nargs=0 AiderSendPromptWithInput call denops#notify("${denops.name}", "sendPromptWithInput", [input("Prompt: ")])`,
   );
   await denops.cmd(
     `command! -nargs=0 AiderRun call denops#notify("${denops.name}", "runAider", [])`,
+  );
+  await denops.cmd(
+    `command! -nargs=0 AiderAddCurrentFile call denops#notify("${denops.name}", "addCurrentFile", [])`,
   );
   await denops.cmd(
     `command! -nargs=0 AiderExit call denops#notify("${denops.name}", "exitAider", [])`,
