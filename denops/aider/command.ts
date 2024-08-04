e55b333631e96549a7f84979224a791fb058063d
nekowasabi <nolifeking00@gmail.com>
2024-08-04 11:21:40 +0900

    refactor: aiderのコマンドをモジュールとして抽出 :construction:

--- denops/aider/command.ts
+++ denops/aider/command.ts
@@ -1,14 +1,7 @@
 import { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
-import { getAiderBufferNr } from "./utils.ts";
 
-export class Command {
-  constructor(private denops: Denops) {
-  }
-
-  async exit(): Promise<void> {
-    const bufnr = await getAiderBufferNr(this.denops);
-    if (bufnr !== undefined) {
-      await this.denops.cmd(`${bufnr}bdelete!`);
-    }
-  }
-}
+export const command = {
+  async debug(denops: Denops): Promise<void> {
+    await denops.cmd("b#");
+  },
+};
