efdedc4e70e764a1ca0658006d549b6321fe23ba
tsukimizake <shomasd@gmail.com>
2024-09-11 18:58:14 +0900

    pass argCount by hand( ；´。 ｀；)

--- denops/aider/main.ts
+++ denops/aider/main.ts
@@ -9,14 +9,14 @@ import { buffer, BufferLayout } from "./buffer.ts";
  * @returns {Promise<void>}
  */
 export async function main(denops: Denops): Promise<void> {
-  type ImplType =
-    | (() => Promise<void>)
-    | ((arg: unknown) => Promise<void>)
-    | ((args: unknown[]) => Promise<void>);
+  type ArgCount = "0" | "1" | "*";
+  type ImplType<T extends ArgCount> = T extends "0" ? (() => Promise<void>)
+    : T extends "1" ? ((arg: string) => Promise<void>)
+    : ((args: string[]) => Promise<void>);
 
   type Command = {
     methodName: string;
-    impl: ImplType;
+    impl: ImplType<ArgCount>;
   };
 
   /**
@@ -28,9 +28,10 @@ export async function main(denops: Denops): Promise<void> {
    * @param {boolean} [range=false] - コマンドがVisualモードで動作するかどうか。
    * @returns {Promise<Command>} - メソッド名、`command!`宣言、実装を含むコマンドオブジェクト。
    */
-  async function command(
+  async function command<argCount extends ArgCount>(
     dispatcherMethod: string,
-    impl: ImplType,
+    argCount: argCount,
+    impl: ImplType<argCount>,
     pattern: "[<f-args>]" | "[<line1>, <line2>]" | "[]" = "[]",
     range: boolean = false,
   ): Promise<Command> {
@@ -38,12 +39,9 @@ export async function main(denops: Denops): Promise<void> {
 
     const commandName = "Aider" + dispatcherMethod.charAt(0).toUpperCase() +
       dispatcherMethod.slice(1);
-    const argCount = (() => {
-      if (impl.length === 0) return "0";
-      if (impl.length === 1) return "1";
-      else return "*";
-    })();
-
+    console.log(
+      `command! -nargs=${argCount} ${rangePart} ${commandName} call denops#notify("${denops.name}", "${dispatcherMethod}", ${pattern})`,
+    );
     await denops.cmd(
       `command! -nargs=${argCount} ${rangePart} ${commandName} call denops#notify("${denops.name}", "${dispatcherMethod}", ${pattern})`,
     );
@@ -62,7 +60,7 @@ export async function main(denops: Denops): Promise<void> {
    */
   function dispatchOnly(
     dispatcherMethod: string,
-    impl: ImplType,
+    impl: ImplType<ArgCount>,
   ): Command {
     return {
       methodName: dispatcherMethod,
@@ -75,39 +73,46 @@ export async function main(denops: Denops): Promise<void> {
   const commands: Command[] = [
     await command(
       "sendPrompt",
+      "0",
       () => buffer.sendPrompt(denops, openBufferType),
     ),
-    await command("run", async () => {
+    await command("run", "0", async () => {
       if (await buffer.openAiderBuffer(denops, openBufferType)) {
         return;
       }
       await aiderCommand.run(denops);
     }),
-    await command("silentRun", () => aiderCommand.silentRun(denops)),
+    await command("silentRun", "0", () => aiderCommand.silentRun(denops)),
     dispatchOnly(
       "sendPromptWithInput",
       () => buffer.sendPromptWithInput(denops),
     ),
-    await command("addFile", async (path: unknown) => {
+    await command("addFile", "1", async (path: string) => {
       const prompt = `/add ${path}`;
       await v.r.set(denops, "q", prompt);
       await denops.dispatcher.sendPromptWithInput(path);
     }, "[<f-args>]"),
-    await command("addCurrentFile", () => aiderCommand.addCurrentFile(denops)),
-    await command("addWeb", async (url: unknown) => {
+    await command(
+      "addCurrentFile",
+      "0",
+      () => aiderCommand.addCurrentFile(denops),
+    ),
+    await command("addWeb", "1", async (url: string) => {
       const prompt = `/web ${url}`;
       await v.r.set(denops, "q", prompt);
       await denops.dispatcher.sendPromptWithInput(url);
     }, "[<f-args>]"),
-    await command("ask", async (question: unknown) => {
+    await command("ask", "1", async (question: string) => {
       const prompt = `/ask ${question}`;
       await v.r.set(denops, "q", prompt);
       await denops.dispatcher.sendPromptWithInput(question);
     }, "[<f-args>]"),
-    await command("exit", () => buffer.exitAiderBuffer(denops)),
+    await command("exit", "0", () => buffer.exitAiderBuffer(denops)),
     await command(
       "openFloatingWindowWithSelectedCode",
-      async ([start, end]: unknown[]) => {
+      "*",
+      async ([start, end]: string[]) => {
+        console.log([start, end]);
         await buffer.openFloatingWindowWithSelectedCode(
           denops,
           start,
@@ -118,13 +123,14 @@ export async function main(denops: Denops): Promise<void> {
       "[<line1>, <line2>]",
       true,
     ),
-    await command("openIgnore", () => aiderCommand.openIgnore(denops)),
+    await command("openIgnore", "0", () => aiderCommand.openIgnore(denops)),
     await command(
       "addIgnoreCurrentFile",
+      "0",
       () => aiderCommand.addIgnoreCurrentFile(denops),
     ),
-    await command("debug", () => aiderCommand.debug(denops)),
-    await command("hide", async () => {
+    await command("debug", "0", () => aiderCommand.debug(denops)),
+    await command("hide", "0", async () => {
       await denops.cmd("close!");
       await denops.cmd(`silent! e!`);
     }),
