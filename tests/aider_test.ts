46140afdf1aaa71475b56305d321ff5c07c6b37d
tsukimizake <shomasd@gmail.com>
2024-10-01 22:38:13 +0900

    add error message

--- tests/aider_test.ts
+++ tests/aider_test.ts
@@ -4,4 +4,20 @@ test("nvim", "AiderRun should work", async (denops) => {
   await denops.cmd('source "setting.vim"');
   await denops.cmd('let g:aider_command = "mockServer.ts"');
   await denops.cmd("AiderRun");
+
+  //  AiderRun should work ... FAILED (131ms)
+  //
+  //   ERRORS
+  //
+  //  AiderRun should work => https://jsr.io/@denops/test/3.0.4/tester.ts:148:10
+  //  error: Error: Failed to call 'denops#api#cmd' in Neovim: function denops#api#cmd, line 2: Vim:E492: エディタのコマンドではありません: AiderRun (code: 0)
+  //      at <anonymous> (ext:core/01_core.js:778:34)
+  //
+  //   FAILURES
+  //
+  //  AiderRun should work => https://jsr.io/@denops/test/3.0.4/tester.ts:148:10
+  //
+  //  FAILED | 0 passed | 1 failed (133ms)
+  //
+  //  error: Test failed
 });
