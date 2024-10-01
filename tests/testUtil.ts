2b99794bc8b553946b0da52497fbf27e28a47767
tsukimizake <shomasd@gmail.com>
2024-10-02 00:20:29 +0900

    tests are working

---
+++ tests/testUtil.ts
@@ -0,0 +1,34 @@
+import { fromFileUrl } from "https://deno.land/std@0.217.0/path/mod.ts";
+import { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
+import { BufferLayout } from "../denops/aider/buffer.ts";
+import { test } from "jsr:@denops/test";
+
+async function setup(denops: Denops, bufferLayout: BufferLayout) {
+  const runtimepath = fromFileUrl(import.meta.resolve("../"));
+  await denops.cmd(`set runtimepath^=${runtimepath}`);
+  await denops.cmd("call denops#plugin#discover()");
+  await denops.cmd('let g:aider_command = "mockServer.ts"');
+  await denops.cmd(`let g:aider_buffer_open_type = "${bufferLayout}"`);
+  const sleep = (msec: number) =>
+    new Promise((resolve) => setTimeout(resolve, msec));
+  await sleep(10); // sleepを入れないとAiderAddCurrentFileが落ちた。mainのロードが間に合っていない？
+}
+
+export function testFloating(
+  testName: string,
+  fn: (denops: Denops) => Promise<void>,
+) {
+  test("nvim", testName, async (denops) => {
+    await setup(denops, "floating");
+    await fn(denops);
+  });
+}
+export function testBuffer(
+  testName: string,
+  fn: (denops: Denops) => Promise<void>,
+) {
+  test("nvim", testName, async (denops) => {
+    await setup(denops, "vsplit");
+    await fn(denops);
+  });
+}
