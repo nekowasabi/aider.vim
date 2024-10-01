b37c7b21273c608af7014c7f230d5ef93adca83e
tsukimizake <shomasd@gmail.com>
2024-10-02 00:29:41 +0900

    fix mockServer path

--- tests/testUtil.ts
+++ tests/testUtil.ts
@@ -7,7 +7,9 @@ async function setup(denops: Denops, bufferLayout: BufferLayout) {
   const runtimepath = fromFileUrl(import.meta.resolve("../"));
   await denops.cmd(`set runtimepath^=${runtimepath}`);
   await denops.cmd("call denops#plugin#discover()");
-  await denops.cmd('let g:aider_command = "mockServer.ts"');
+  await denops.cmd(
+    `let g:aider_command = "${runtimepath}tests/mockServer.ts"`,
+  );
   await denops.cmd(`let g:aider_buffer_open_type = "${bufferLayout}"`);
   const sleep = (msec: number) =>
     new Promise((resolve) => setTimeout(resolve, msec));
