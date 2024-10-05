eaf8fc9244b25392b565c367976a901c4f719d18
tsukimizake <shomasd@gmail.com>
2024-10-05 14:46:26 +0900

    rename: testUtil -> testRunner

--- tests/aider_test.ts
+++ tests/aider_test.ts
@@ -1,5 +1,5 @@
 // This comment for vim-test plugin: Use denops' test() instead of built-in Deno.test()
-import { test } from "./testUtil.ts";
+import { test } from "./testRunner.ts";
 
 test("floating", "AiderRun should work", async (denops) => {
   await denops.cmd("AiderRun");
