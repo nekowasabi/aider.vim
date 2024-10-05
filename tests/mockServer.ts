adc7cde59c1ad3bfec11cc51b1ea17b2053e58c6
tsukimizake <shomasd@gmail.com>
2024-10-05 14:38:29 +0900

    rm mockServer.ts

--- tests/mockServer.ts
+++
@@ -1,8 +0,0 @@
-const decoder = new TextDecoder();
-const encoder = new TextEncoder();
-
-for await (const buf of Deno.stdin.readable) {
-  const input = decoder.decode(buf);
-  const output = `input is ${input}`;
-  await Deno.stdout.write(encoder.encode(output));
-}
