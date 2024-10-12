bf2a0df33244aba222086d7825527867d9ac5ffd
tsukimizake <shomasd@gmail.com>
2024-10-12 17:01:31 +0900

    Create deno.yml, longer sleep

--- tests/aider_test.ts
+++ tests/aider_test.ts
@@ -5,26 +5,26 @@ import { test } from "./testRunner.ts";
 
 test("floating", "AiderRun should work", async (denops) => {
   await denops.cmd("AiderRun");
-  await sleep(10);
+  await sleep(100);
   await assertAiderBufferShown(denops);
 });
 
 test("vsplit", "AiderRun should work", async (denops) => {
   await denops.cmd("AiderRun");
-  await sleep(10);
+  await sleep(100);
   await assertAiderBufferShown(denops);
 });
 
 test("floating", "AiderAddCurrentFile should work", async (denops) => {
   await denops.cmd("AiderAddCurrentFile");
-  await sleep(10);
+  await sleep(100);
   await assertAiderBufferAlive(denops);
   await assertAiderBufferString(denops, "input: /add \n");
 });
 
 test("vsplit", "AiderAddCurrentFile should work", async (denops) => {
   await denops.cmd("AiderAddCurrentFile");
-  await sleep(10);
+  await sleep(100);
   await assertAiderBufferAlive(denops);
   await assertAiderBufferString(denops, "input: /add \n");
 });
