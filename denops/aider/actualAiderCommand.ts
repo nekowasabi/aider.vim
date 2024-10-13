adc4784356e3a994e3e669c055d0b37286b53b09
tsukimizake <shomasd@gmail.com>
2024-10-13 20:43:27 +0900

    remove silentRun from AiderCommand

--- denops/aider/actualAiderCommand.ts
+++ denops/aider/actualAiderCommand.ts
@@ -8,7 +8,6 @@ import * as util from "./utils.ts";
 
 export const commands: AiderCommand = {
   run,
-  silentRun,
   sendPrompt,
   exit,
   checkIfAiderBuffer,
@@ -20,10 +19,7 @@ export const commands: AiderCommand = {
  * @param {number} bufnr - バッファ番号
  * @returns {Promise<boolean>}
  */
-async function checkIfAiderBuffer(
-  denops: Denops,
-  bufnr: number,
-): Promise<boolean> {
+async function checkIfAiderBuffer(denops: Denops, bufnr: number): Promise<boolean> {
   // aiderバッファの場合 `term://{path}//{pid}:aider --4o --no-auto-commits` のような名前になっている
   const name = await util.getBufferName(denops, bufnr);
   const splitted = name.split(" ");
@@ -31,34 +27,9 @@ async function checkIfAiderBuffer(
 }
 
 async function run(denops: Denops): Promise<undefined> {
-  const aiderCommand = ensure(
-    await v.g.get(denops, "aider_command"),
-    is.String,
-  );
+  const aiderCommand = ensure(await v.g.get(denops, "aider_command"), is.String);
   await denops.cmd(`terminal ${aiderCommand}`);
   await emit(denops, "User", "AiderOpen");
-}
-
-/**
- * Aiderをバックグラウンドで実行します。
- * 新しいバッファを作成し、Aiderコマンドをターミナルで実行した後、
- * 前のバッファに戻ります。
- * @param {Denops} denops - Denopsインスタンス
- * @returns {Promise<undefined>}
- */
-async function silentRun(denops: Denops): Promise<undefined> {
-  await denops.cmd("enew");
-
-  const aiderCommand = ensure(
-    await v.g.get(denops, "aider_command"),
-    is.String,
-  );
-  await denops.cmd(`terminal ${aiderCommand}`);
-  await emit(denops, "User", "AiderOpen");
-
-  await denops.cmd("b#");
-
-  await denops.cmd("echo 'Aider is running in the background.'");
 }
 
 /**
@@ -68,22 +39,14 @@ async function silentRun(denops: Denops): Promise<undefined> {
  * @param {string} prompt - The prompt to send
  * @returns {Promise<undefined>}
  */
-async function sendPrompt(
-  denops: Denops,
-  jobId: number,
-  prompt: string,
-): Promise<undefined> {
+async function sendPrompt(denops: Denops, jobId: number, prompt: string): Promise<undefined> {
   await v.r.set(denops, "q", prompt);
   await fn.feedkeys(denops, "G");
   await fn.feedkeys(denops, '"qp');
   await denops.call("chansend", jobId, "\n");
 }
 
-async function exit(
-  denops: Denops,
-  jobId: number,
-  bufnr: number,
-): Promise<undefined> {
+async function exit(denops: Denops, jobId: number, bufnr: number): Promise<undefined> {
   await denops.call("chansend", jobId, "/exit\n");
   await denops.cmd(`bdelete! ${bufnr}`);
 }
